8d01d8d321f9e69a130326d25034a54feeaaee77
marvel <adamarvel7@gmail.com>
2024-08-27 15:04:14 +0100

    known

---
+++ script/comparisonoperator.js
@@ -0,0 +1,21 @@
+var name= 'john'
+var age= '18'
+console.log(name=='john')
+// console.log(name=='james')
+// var firstName= prompt('please enter your name')
+// console.log(name==firstName)
+// console.log(name !='john')
+// console.log(name!='james')
+// console.log(age>18)
+// console.log(age>=18)
+// console.log(age<18)
+// console.log(age<=18)
+price  =     10
+newprice  = '10'
+console.log(price==newprice)
+console.log(price===newprice)
+console.log(typeof(price))
+console.log(typeof(newprice))
+//note this is
+// = actual
+// =interrogative
--- script/operator.js
+++ script/operator.js
@@ -1,1 +1,15 @@
-var known= ''
+var price= 50;
+var price2= 2;
+var add= price+price2;
+console.log(add);
+var subtract= price-price2;
+console.log(subtract);
+var divide= price/price2;
+console.log(divide);
+var mutiply= price*price2;
+console.log(mutiply);
+var exponential= price**price2;
+console.log(exponential);
+var modules= price%price2;
+console.log(modules);
+console.log(5**2)

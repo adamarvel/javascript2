70cca76587939046ba959a1891354afb9ae247ba
marvel <adamarvel7@gmail.com>
2024-08-27 10:25:58 +0100

    new dimensions

---
+++ script/array.js
@@ -0,0 +1,34 @@
+var nums =[1,2,3,4,5]
+console.log(nums)
+var friend=['fred','tobi','fidel' , 'nancy','bukola']
+friend.push('marvel')
+console.log(friend)
+friend.pop()
+console.log(friend)
+console.log(friend)
+console.log(friend.length)
+console.log(friend[0])
+friend[0] = 'criminals'
+console.log(friend[0])
+console.log(friend)
+friend[3] = 'marvel'
+console.log(friend[3])
+console.log(friend)
+friend.unshift('human')
+console.log(friend)
+friend.shift('human')
+console.log(friend)
+var student = {
+    name: 'john',
+    age: 18,
+    course: 'js'
+}
+console.log(`${student.name} is ${student.age} years old studying ${student.course}`)
+console.log(student)
+console.log(student.name)
+console.log(student.name + ' is '+student.age+ ' years studying '+student.course)
+var name= 'wealth'
+var age = 18
+console.log(name+ ' is ' +age+ ' years old')
+var myName= prompt('please enter your stupid name')
+alert('welcome you idiot now enjoy my beautiful website, '+myName)
